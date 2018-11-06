5ee0fe735307ef306d452fe9af4d33b5a1feda83
Voltinus <voltinusmail@gmail.com>
2018-11-06 08:31:57 +0100

    Added stateMenu

---
+++ stateMenu.c
@@ -0,0 +1,56 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdbool.h>
+#include "events.h"
+#include "tick.h"
+#include "render.h"
+#include "states.h"
+
+
+void menuEvents() {
+  SDL_Event e;
+  
+  while(SDL_PollEvent(&e)) {
+    if(e.type == SDL_QUIT) quit = true;
+  }
+}
+
+void menuTick() {
+  char str[12];
+  sprintf(str, "%d", tickNo);
+  write(1, 1, str, 0xFFF, 0);
+
+  tickNo++;
+}
+
+void menuRedraw() {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  for(uint32_t i=0; i<SCREEN_W * SCREEN_H * 5; i += 5) {
+    if(screen[i] == 0) continue;
+
+    SDL_Rect src;
+    src.x = (screen[i]%16)*tileW; src.w = tileW;
+    src.y = (screen[i]/16)*tileH; src.h = tileH;
+
+    SDL_Rect dst;
+    dst.x = ((i/5)%SCREEN_W)*tileW*zoom; dst.w = tileW*zoom;
+    dst.y = ((i/5)/SCREEN_W)*tileH*zoom; dst.h = tileH*zoom;
+
+    uint8_t r = (screen[i+1] & 0x0F)*17,
+            g = (screen[i+2] >> 4)*17,
+            b = (screen[i+2] & 0x0F)*17;
+  
+    SDL_SetTextureColorMod(tileset, r, g, b);
+    SDL_RenderCopy(renderer, tileset, &src, &dst);
+  }
+  
+  SDL_RenderPresent(renderer);
+}
+
+State menuState = {
+  .events = menuEvents,
+  .tick   = menuTick,
+  .redraw = menuRedraw
+};
